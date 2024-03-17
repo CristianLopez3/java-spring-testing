5d185706bb127a911fcd7c2b12d124b6750056bb
cristian <cristian.c.lopez.m@gmail.com>
2024-03-17 17:25:55 -0500

    ADD: disabling test and dependent assertions

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Java Testing
 
-Unit testing and best practices with java and spring boot
+Unit testing and best practices with java and spring boot.
+
+>![NOTE]
+>You can enter in every folder, I put notes to understand and help me just in case.
---
+++ testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+
+  @Test
+  void dependentAssertions(){
+    Owner owner = new Owner(1l, "Cristian", "Lopez");
+    owner.setCity("Bogota");
+    owner.setAddress("Calle 123");
+
+    assertAll("Properties Test",
+            () -> assertAll("Person Properties",
+                    () -> assertEquals("Cristian", owner.getFirstName(), "First name did not match"),
+                    () -> assertEquals("Lopez", owner.getLastName(), "Last name did not match")
+            ),
+            () -> assertAll("Owner Properties",
+                    () -> assertEquals("Bogota", owner.getCity(), "City did not match"),
+                    () -> assertEquals("Calle 123", owner.getAddress(), "Address did not match"))
+    );
+  }
+
+}
