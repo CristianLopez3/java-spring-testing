701a145e3cc976e6302c891ea19b966eb1eb7812
cristian <cristian.c.lopez.m@gmail.com>
2024-03-24 19:04:09 -0500

    csv files with parametrized test

--- testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,9 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -33,10 +37,39 @@ class OwnerTest implements ModelTest {
   }
 
 
-  @ParameterizedTest
+  @DisplayName("Test Value Source")
+  @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
   @ValueSource(strings = {"Spring", "Framework", "Crisitan"})
   void parametrizedTest(String value){
     System.out.println(value);
+  }
+
+
+  @DisplayName("Owner Type Test")
+  @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+  @EnumSource(OwnerType.class)
+  void OwnerTypeTest(OwnerType values){
+    System.out.println(values);
+  }
+
+
+  @DisplayName("Csv Input Test")
+  @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+  @CsvSource({
+          "FL, 1, 1",
+          "OH, 2, 2",
+          "MI, 3, 1"
+  })
+  void csvInputTest(String stateName, int val1, int val2){
+    System.out.println(stateName + " = " + val1 + ":" + val2);
+  }
+
+
+  @DisplayName("Csv From File Test")
+  @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+  @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+  void csvFromFileTest(String stateName, int val1, int val2){
+    System.out.println(stateName + " = " + val1 + ":" + val2);
   }
 
 }
